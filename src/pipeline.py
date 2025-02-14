b85ea2a0b90ba3dac2374fcbb1ce3a5d17a9aef6
FelipeStillner <felipeeufranio@gmail.com>
2025-02-13 22:25:10 -0300

    feat(): merge plate rekognition pipeline in radxa code

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,20 +1,22 @@
+import neural_network.index as neural_network
 import asyncio
 import lcd
 import database
 import buzzer
 import camera
 
+# TODO: Test detect_plate function with and without the pipeline
 async def detect_plate():
     global global_frame
     while (1):
         await asyncio.sleep(0.1)
-        # TODO: detect real plate
-        plate = "ABC1D23"
-        has_log, type, severity = database.get_plate_from_database(plate)
-        if (has_log):
-            camera.init_recording()
-            lcd.escrever_lcd(f"{plate} {severity}", f"* {type}")
-            await buzzer.buzzer()
+        plate, accuracy = neural_network.license_plate_recognition_pipeline(global_frame)
+        if accuracy > 0.5:
+            has_log, type, severity = database.get_plate_from_database(plate)
+            if (has_log):
+                camera.init_recording()
+                lcd.escrever_lcd(f"{plate} {severity}", f"* {type}")
+                await buzzer.buzzer()
         
         
         
